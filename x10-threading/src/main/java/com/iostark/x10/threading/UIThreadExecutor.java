16b989e7881221ce6bf7b97150a5b6c8b61504c2
David Andreoletti <david.andreoletti.dev@gmail.com>
2017-08-12 09:58:10 +0800

    Added UIThreadExecutor

---
+++ x10-threading/src/main/java/com/iostark/x10/threading/UIThreadExecutor.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2015 IO Stark
+ */
+
+package com.iostark.x10.threading;
+
+import android.os.Handler;
+import android.os.Looper;
+
+import com.iostark.x10.base.annotation.api.API_Internal;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.RejectedExecutionException;
+
+/**
+ *
+ */
+@API_Internal
+public class UIThreadExecutor implements Executor {
+    private static final Handler HANDLER = new Handler(Looper.getMainLooper());
+
+    @Override
+    @SuppressWarnings("PMD.AvoidThrowingNullPointerException")
+    public void execute(final Runnable command) {
+        if (command == null) {
+            throw new NullPointerException();
+        }
+
+        final boolean isInQueue = HANDLER.post(command);
+
+        if (!isInQueue) {
+            throw new RejectedExecutionException();
+        }
+    }
+
+}
